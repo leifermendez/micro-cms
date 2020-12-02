2240a82b8a0b3d63b5eb27929baa20498ad8c1eb
Leifer Mendez <leifer33@gmail.com>
2020-12-02 12:14:21 +0100

    Fix secure

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if($this->app->environment('production')) {
-//            \URL::forceScheme('https');
+            \URL::forceScheme('https');
         }
     }
 }
